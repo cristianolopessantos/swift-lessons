7125f7beb767d5d9e5a5c676c8bf807948509177
cristianolopessantos <cristianolopes@dcc.ufrj.br>
2022-08-20 10:48:46 -0300

    Introdução de outlets e actions

--- Storyboard/ViewController.swift
+++ Storyboard/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet weak var button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func button(_ sender: Any) {
+        label.text = "Hello darkness"
+        button.self.setTitle("Apertei IBAction", for: .normal)
+    }
 
 }
 
